235b84a2ceba4e83602e34ddd12163e902aa617c
AutocorrectGuy <103662175+AutocorrectGuy@users.noreply.github.com>
2022-09-04 11:39:57 +0300

    :// for axios prod url appened

--- frontend/src/services/axios/axios.config.ts
+++ frontend/src/services/axios/axios.config.ts
@@ -3,7 +3,7 @@ import axios, { AxiosInstance, AxiosRequestConfig } from 'axios';
 const axiosConf: AxiosInstance = axios.create({
   baseURL: !process.env.NODE_ENV || process.env.NODE_ENV === 'development'
     ? "http://localhost:3001/"
-    : `${window.location.href.split(":")[0]}training-003.herokuapp.com/`,
+    : `${window.location.href.split(":")[0]}://training-003.herokuapp.com/`,
   withCredentials: true
 });
 
